694ca2627350f2b21c763c00b34bf666ceed9af2
enbu16385 <enbu16385@gmail.com>
2026-05-26 21:38:35 +0900

    更新

--- docs/react-app/vite.config.ts
+++ docs/react-app/vite.config.ts
@@ -1,15 +1,36 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
-  // 安全にJSONとして埋め込む
   const gitHistory = env.GIT_HISTORY ? env.GIT_HISTORY : '[]';
   const gitUpdatedAt = env.GIT_UPDATED_AT ? env.GIT_UPDATED_AT : '{}';
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      sitemap({
+        hostname: 'https://enbu-kouryaku.com', // ← ここだけ変える
+        dynamicRoutes: [
+          '/',
+          '/SkillRoot',
+          '/SgFree',
+          '/SerialCode',
+          '/ReviewTensho',
+          '/ReviewShinsho',
+          '/ReviewHaou',
+          '/ReviewRaid',
+          '/ReviewKassen',
+          '/DataSkill',
+          '/DataOugi',
+          '/DataOther',
+          '/RaidStatistics',
+          '/DeckRaid',
+        ],
+      }),
+    ],
     build: {
       outDir: 'dist',
     },
